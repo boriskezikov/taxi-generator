a749da7b70bdba9ab0b8bfee187f6c25d0f2c94b
kezik <kezikov@vtb.ru>
2021-03-15 18:59:51 +0300

    heroku конфиг

--- src/main/java/ru/taxi/generator/generator/GeneratorAccessorService.java
+++ src/main/java/ru/taxi/generator/generator/GeneratorAccessorService.java
@@ -3,6 +3,7 @@ package ru.taxi.generator.generator;
 import com.google.maps.model.GeocodingResult;
 import com.google.maps.model.LatLng;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import ru.taxi.generator.geoapi.GoogleApiGeoEncoderGoogle;
@@ -12,6 +13,7 @@ import ru.taxi.generator.geoapi.GoogleApiRetriever;
 import java.math.BigInteger;
 import java.time.LocalDate;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GeneratorAccessorService {
@@ -35,6 +37,7 @@ public class GeneratorAccessorService {
     }
 
     public GeneratorParametersEntity loadParameters() {
+        log.info("Loading parameters");
         return generatorParamsRepository.findAll().stream().findFirst().orElse(null);
     }
 
